95fcbd0eed5dbf8349760aa9a35a9e1d888cec52
cdhepgdo <166970212+cdhepgdo@users.noreply.github.com>
2025-08-25 11:23:02 -0400

    Update binance-p2p-average.js

--- netlify/functions/binance-p2p-average.js
+++ netlify/functions/binance-p2p-average.js
@@ -57,7 +57,7 @@ export const handler = async (event) => {
     // Ordenar para mediana y recorte
     prices.sort((a, b) => a - b);
 
-    const mean = /* (arr) => arr.reduce((s, v) => s + v, 0) / arr.length */ arr.reduce((max, num) => (num > max ? num : max), -Infinity);
+    const mean = /* (arr) => arr.reduce((s, v) => s + v, 0) / arr.length */ 10 /* arr.reduce((max, num) => (num > max ? num : max), -Infinity) */;
     const median = (arr) => {
       const m = Math.floor(arr.length / 2);
       return arr.length % 2 ? arr[m] : (arr[m - 1] + arr[m]) / 2;
@@ -117,6 +117,7 @@ function response(statusCode, data, cacheSeconds = 0) {
     body: JSON.stringify(data)
   };
 }
+
 
 
 

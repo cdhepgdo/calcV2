2cfad7477d76851bb381d140112d6cd971bc0c18
cdhepgdo <166970212+cdhepgdo@users.noreply.github.com>
2026-03-21 16:32:54 -0400

    Update registro.js

--- public/js/registro.js
+++ public/js/registro.js
@@ -679,7 +679,7 @@ class RegistroDiario {
             }
             
             const fechaLimite = new Date();
-            fechaLimite.setDate(fechaLimite.getDate() - periodo);
+            fechaLimite.setDate(fechaLimite.getDate() - periodoVal);
             
             diasFiltrados = diasFiltrados.filter(dia => {
                 const fechaDia = this.parsearFecha(dia.fecha);

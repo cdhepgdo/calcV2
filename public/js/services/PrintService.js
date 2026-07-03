98f4d748b3e7639d1fff1de971d86a9a12428e16
cdhepgdo <166970212+cdhepgdo@users.noreply.github.com>
2026-07-03 10:37:50 -0400

    Update logo image source in PrintService.js

--- public/js/services/PrintService.js
+++ public/js/services/PrintService.js
@@ -396,7 +396,7 @@ class PrintService {
 
                 <!-- Figura superior -->
                 <div class="figure" style="right: 79px;">
-                    <img src="${logoBase64 || '/img/width_200.webp'}" alt="logo">
+                    <img src="${window.location.origin}/img/width_200.webp" alt="logo">
                 </div>
             </div>
 
